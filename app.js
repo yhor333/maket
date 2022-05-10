0704bbf580009f3fcb7005da841ba69d66c6a9dc
yhor333 <yhor450@gmail.com>
2022-05-10 16:35:45 +0300

    m

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-function MathChallenge(num) {
-  const intengers = new Array(num).fill(1);
-  const result = intengers.reduce((acc, _, index) => acc + index + 1, 0);
-  return result;
-}
 
-// keep this function call here
-console.log(MathChallenge(readline()));
